4f7c46c1d622049a7bfb6b351fd5b7ad483d8ab0
BigMonster <lzx1994@sina.com>
2016-02-01 09:08:40 +0800

    nonlinear 3

--- funmin.m
+++ funmin.m
@@ -1,12 +1,27 @@
-function [cost]=funmin(x)
+function [cost,g,H]=funmin(x)
 load Init avr_price TotalNum Risk earning index
 NN=diag(TotalNum.*Risk.*earning*1.2);
 NN=NN(index,index);
 ss=sfunc(x,avr_price(index)/2);
 cost=sum(NN*ss);
+if nargout>1
+    g=gfunc(x,avr_price(index)/2);
+    if nargout>2
+        H=diag(g2func(x,avr_price(index)/2));
+    end
+end
+        
 end
 
 function [ss]=sfunc(x,u)
 % ss=-1/7*(x./u-1).^7+3/5*(x./u-1).^5-(x./u-1).^3+(x./u-1);
 ss=(-1/7*(x./u-1).^7+3/5*(x./u-1).^5-(x./u-1).^3+(x./u-1)+16/35)*35/32;
+end
+
+function [g]=gfunc(x,u)
+    g=(-(x./u-1).^6+3*(x./u-1).^4-3*(x./u-1).^2+1)./u*35/32;
+end
+
+function [gg]=g2func(x,u)
+    gg=(-6*(x./u-1).^5+12*(x./u-1).^3-6*(x./u-1))./u./u*35/32;
 end
--- nonlinear.m
+++ nonlinear.m
@@ -5,5 +5,17 @@ y=t-t.^3+3/5*t^5-1/7*t^7;
 t=-1:0.01:1;
 val=subs(y);
 %}
-
-fmincon('funmin',x1,A',b,[],[],zeros(N,1),avr_price(index));
+% options=optimset('GradObj','on');
+options=optimset('GradObj','on','Hessian','on');
+% x2=fmincon('funmin',x1,A',b,[],[],zeros(N,1),avr_price(index),[],options);
+x2=fmincon('funmin',x1,[],[],[],[],zeros(N,1),[],[],options);
+%%
+[s_v,s_o]=sort(x2,'descend');
+s_v(1:20)        % rank of personal investment    
+uid(s_o(1:20))
+sum_value=NN*x2; % rank of total investment of each school
+sum_value(s_o(1:20))
+temp=TotalNum(index);
+temp(s_o(1:20))
+temp2=A'*x2;
+-temp2(2:7)
